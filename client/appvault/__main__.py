c65d4838bf725dda969316cd0f3f3403d2341260
Maku <makusu2@gmail.com>
2019-03-13 15:17:51 -0400

    Tidied up argparse functionality

--- client/appvault/__main__.py
+++ client/appvault/__main__.py
@@ -5,10 +5,19 @@ import comms
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Interface with Appvault")
-    parser.add_argument("-e", "--encrypt", help="Encryption mode",
-                        action="store_true")
-    parser.add_argument("-o", "--outfile", help="Specify outfile", type=Path)
-    parser.add_argument("infile", type=Path)
+    runmode_group = parser.add_mutually_exclusive_group()
+    runmode_group.add_argument("-e", "--encrypt", action="store_true",
+                               help=("Encryption mode (encrypt infile and "
+                                     "store in outfile (if supplied))"))
+    runmode_group.add_argument("-r", "--run", action="store_true",
+                               help=("Run mode (run encrypted infile). "
+                                     "Default mode."))
+    parser.add_argument("-o", "--outfile", type=Path,
+                        help="File that encrypted data should be written to")
+    parser.add_argument("infile", type=Path,
+                        help=("File name to use (file to encrypt if "
+                              "--encrypt, else file to run"))
+
     args = parser.parse_args()
 
     if args.encrypt:
